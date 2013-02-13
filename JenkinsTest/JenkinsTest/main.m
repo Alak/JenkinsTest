6e32c00ccbfe591380f4c90cdc1b3b69e59e9374
Alak <alakme@gmail.com>
2013-02-13 14:25:46 +0100

    initial commit

---
+++ JenkinsTest/JenkinsTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JenkinsTest
+//
+//  Created by CATHALY Kevin on 13/02/13.
+//  Copyright (c) 2013 Iléo Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ITAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ITAppDelegate class]));
+    }
+}
---
+++ JenkinsTest/JenkinsTestTests/JenkinsTestTests.h
@@ -0,0 +1,13 @@
+//
+//  JenkinsTestTests.h
+//  JenkinsTestTests
+//
+//  Created by CATHALY Kevin on 13/02/13.
+//  Copyright (c) 2013 Iléo Technologies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JenkinsTestTests : SenTestCase
+
+@end
